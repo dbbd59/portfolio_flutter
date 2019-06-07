d349fd2a466615c43622fb0905d0183331c1f906
davide <davide@w-her.com>
2019-06-07 16:54:13 +0200

    changes...

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class ThemeProvider extends ChangeNotifier {
   Brightness brightnessTheme = Brightness.dark;
   setBrightness(Brightness brightness) {
-    print(brightness);
     brightnessTheme = brightness;
     notifyListeners();
   }
